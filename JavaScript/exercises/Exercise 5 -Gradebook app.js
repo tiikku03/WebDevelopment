2d3e9db1f039f6c28d9f04dc75fde3c386a9fa40
Victor <victorticu2003@gmail.com>
2025-05-07 12:39:09 -0600

    changes made long ago to these files

--- JavaScript/exercises/Exercise 5 -Gradebook app.js
+++ JavaScript/exercises/Exercise 5 -Gradebook app.js
@@ -53,3 +53,6 @@ function studentMsg(totalScores, studentScore) {
 
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
 console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
+
+
+console.log()
---
+++ JavaScript/exercises/Exercise 6.js
@@ -0,0 +1,30 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const l1 = [2,4,3];
+const l2 = [5,6,4];
+
+var addTwoNumbers = function(l1, l2) {
+    
+};
+
+let num = "123"
+
