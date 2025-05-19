aa113628d5562262ac5abc8f0fef1dc0f2f7f719
Victor Emanuel <victorticu2003@gmail.com>
2025-05-19 14:24:41 -0600

    created two new files that are going to be used in the  JavaScript lectures

---
+++ FreeCodeCamp-HTML/JavaScript/Lectures/Lecture 4 Arrays.js
@@ -0,0 +1,12 @@
+/* 
+.map()
+.join()
+.sort()
+.find() returns undefined if the item is not found within the array
+.indexOf()
+
+? used in -->> function(object?.property); <<--makes to return undefined instead of an error
+
+.classList is used to access the class of a tag in the html file
+
+*/
